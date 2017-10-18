dd6274e04a48cb2bdb411c2fb3959e67d4c71e4d
FVI <test@examle.com>
2017-10-18 09:29:45 -0400

    adding a lulu_blog module db cnection

---
+++ Lulu/Blog/Model/ResourceModel/Post/Collection.php
@@ -0,0 +1,20 @@
+<?php namespace Lulu\Blog\Model\ResourceModel\Post;
+
+class Collection extends \Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection
+{
+    /**
+     * @var string
+     */
+    protected $_idFieldName = 'post_id';
+
+    /**
+     * Define resource model
+     *
+     * @return void
+     */
+    protected function _construct()
+    {
+        $this->_init('Lulu\Blog\Model\Post', 'Lulu\Blog\Model\ResourceModel\Post');
+    }
+
+}
